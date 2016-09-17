673c50a93895842c8100129614a4bfa5b5c9723d
Zarela Velasquez <zarelavp@yahoo.com>
2016-09-16 22:55:52 -0400

    Fixed and updated methods for the game

--- game.js
+++ game.js
@@ -4,37 +4,90 @@
 
 //Player constructor to display dealer and player's status
 
-function Players (bankroll, playerCards, finalPoints){
-	this.bankroll = bankroll;
-	this.playerCards = playerCards;
-	this.finalPoints = finalPoints;
-}
-
-//Function card where value is the card number and points represents card points for the game
-function card(value, name, suit, points){
+function Card(value, name, suit, points){
 	this.value = value;
 	this.name = name;
 	this.suit = suit;
 	this.points = points;
 }
 
-//Counter is position at zero becasue is the position of the card in the shuffle deck.
-var counter = 0;
-//Function to make all cards in a deck
-function deck(){
-	this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-	this.points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-	this.suits = ['Hearts','Diamonds','Spades','Clubs'];
-	var cards = [];
-
-    for( var s = 0; s < this.suits.length; s++ ) {
-        for( var n = 0; n < this.names.length; n++ ) {
-            cards.push( new card( n+1, this.names[n], this.suits[s], this.points[n] ) );
-        }
-    }
-    return cards;
+function Deck(){
+	this.cards = [];
+	this.reset();
 }
 
+Deck.prototype.reset = function(){
+
+	this.cards = [];
+	var names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+	var points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+	var suits = ['Hearts','Diamonds','Spades','Clubs'];
+
+	for( var s = 0; s < suits.length; s++ ) {
+			for( var n = 0; n < names.length; n++ ) {
+					this.cards.push( new Card( n+1, names[n], suits[s], points[n] ) );
+			}
+	}
+}
+
+Deck.prototype.draw = function(number){
+	var drawnCards = [];
+
+	for (var i=0; i<number  ;i++){
+		drawnCards.push(this.cards.pop());
+	}
+	console.log(drawnCards);
+	return drawnCards;
+}
+
+Deck.prototype.shuffle = function(){
+	var deck = this;
+    var shuffledCards = [];
+
+    for( var b = 0; b < deck.cards.length; b++ ) {
+        var randomNumber =  Math.floor(Math.random() * (deck.cards.length ) );
+        shuffledCards.push((deck.cards[randomNumber]));
+    }
+	this.cards = shuffledCards;
+}
+
+//
+//**might be back
+// function Player (bankroll, playerCards, finalPoints){
+// 	this.bankroll = bankroll;
+// 	this.playerCards = playerCards;
+// 	this.finalPoints = finalPoints;
+// }
+//
+// //Function card where value is the card number and points represents card points for the game
+// function Card(value, name, suit, points){
+// 	this.value = value;
+// 	this.name = name;
+// 	this.suit = suit;
+// 	this.points = points;
+// }
+//
+// //Counter is position at zero becasue is the position of the card in the shuffle deck.
+// var counter = 0;
+// //Function to make all cards in a deck
+// function Deck(){
+//
+// 	this.cards = [];
+//
+// }
+//
+// Deck.prototype.reset = function(){
+// 	var names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+// 	var points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+// 	var suits = ['Hearts','Diamonds','Spades','Clubs'];
+//
+// 	for( var s = 0; s < suits.length; s++ ) {
+// 			for( var n = 0; n < names.length; n++ ) {
+// 					this.cards.push( new Card( n+1, names[n], suits[s], points[n] ) );
+// 			}
+// 	}
+// }
+// //** end of be back
 
 var App = {
 	playerCards: [],
@@ -45,77 +98,99 @@ var App = {
   maxBet:90,
   initCredit: 100,
   initBet: 10,
-	shoe: null,
-	//returns all cards shuffled
-  shuffle: function(deckToShuffle=deck()){
-    var shuffledDeck = [];
-    // loop over cards, remove a random card from deck, add it to shuffleddeck array
-      for( var b = 0; b < deckToShuffle.length; b++ ) {
-        // get a random number from 0 to deck.length
-        var randomNumber =  Math.floor(Math.random() * (deckToShuffle.length ) );
-        shuffledDeck.push((deckToShuffle[randomNumber]));
-      }
-			//console.log(shuffledDeck);
-      return shuffledDeck;
 
-  },
+	startGame: function(){
+		deck = new Deck();
+		console.log(deck);
+		deck.shuffle();
+		App.playerCards.push(deck.draw(2));
+		console.log(App.playerCards);
+		App.dealerCards.push(deck.draw(2));
+		console.log(App.dealerCards);
+	}
+
+
+	//returns all cards shuffled
+
+	//*** might be back
+  // shuffle: function(){
+	// 	var deck = new Deck();
+  //   var shuffledCards = [];
+  //   // loop over cards, remove a random card from deck, add it to shuffleddeck array
+  //     for( var b = 0; b < deck.cards.length; b++ ) {
+  //       // get a random number from 0 to deck.length
+  //       var randomNumber =  Math.floor(Math.random() * (deck.cards.length ) );
+  //       shuffledCards.push((deck.cards[randomNumber]));
+  //     }
+	// 		//console.log(shuffledDeck);
+  //     return shuffledCards;
+  // },
+	//end ***
+
 	//Updates cards, bankroll and points for dealer and players
 	//**Be back**
-	setup: function (){
-		App.shoe = App.shuffle();
-		App.player = new Players();
-		App.dealer = new Players();
+	// setup: function (){
+	// 	App.shoe = App.shuffle();
+	// 	App.player = new Player();
+	// 	App.dealer = new Player();
+	//
+  //  console.log(App.shoe);
+	//  	App.deal();
+	// },
 
-   console.log(App.shoe);
-	 	App.deal();
-	},
-
-	deal:function(){
-		// var shuffled = this.shuffle(deck());
-		// counter +=1;
-		// this.playerCards.push(shuffled[counter]);
-		// console.log(this.playerCards[0]);
-		// counter +=1;
-		// this.playerCards.push(shuffled[counter]);
-		// console.log(this.playerCards[1]);
-		App.nextCard();
-		App.nextCard();
-
-	},
-
-  nextCard:function(){
-		var temp = 	App.shoe.pop();
-		App.playerCards.push(temp);
-		console.log(App.playerCards);
-
-  },
-
-  hit: function(){
-  	App.nextCard();
-  },
-
-	stand: function(){
-		//stops player from receiving cards
-		//updates the value of the hand
-  },
-  displayingCards: function(){
-    // once dealer has stoped playing, cards should go to array of points
-    // player cards should go to its own
-  },
-	calculateHandValue: function(){
-	//calculates points from playerCards array
-	},
-
-  decidingWinner: function(){
-    // winner with the accurate points will be declared a winner
-  },
-
-  cleanUp: function(){
-    // game will finish
-    // if user has 0 coins
-    // else if clicks restart
-    // else if quits game
-  },
+	// deal:function(){
+	// 	// var shuffled = this.shuffle(deck());
+	// 	// counter +=1;
+	// 	// this.playerCards.push(shuffled[counter]);
+	// 	// console.log(this.playerCards[0]);
+	// 	// counter +=1;
+	// 	// this.playerCards.push(shuffled[counter]);
+	// 	// console.log(this.playerCards[1]);
+	//
+	// 	//Returns two random cards
+	// 	App.nextCard();
+	// 	App.nextCard();
+	//
+	// },
+	//
+  // nextCard:function(player){
+	// 	var temp = 	App.shoe.pop();
+	// 	App.playerCards.push(temp);
+	// 	// console.log(App.playerCards);
+	//
+  // },
+	//
+  // hit: function(){
+	// 	if((App.playerCards[0].value + App.playerCards[1].value) < 21){
+	// 		App.nextCard();
+	// 	}
+	// 	else {
+	// 		console.log("You lost");
+	// 	}
+  // },
+	//
+	// stand: function(){
+	// 	//stops player from receiving cards
+	// 	//updates the value of the hand
+  // },
+  // displayingCards: function(){
+  //   // once dealer has stoped playing, cards should go to array of points
+  //   // player cards should go to its own
+  // },
+	// calculateHandValue: function(){
+	// //calculates points from playerCards array
+	// },
+	//
+  // decidingWinner: function(){
+  //   // winner with the accurate points will be declared a winner
+  // },
+	//
+  // cleanUp: function(){
+  //   // game will finish
+  //   // if user has 0 coins
+  //   // else if clicks restart
+  //   // else if quits game
+  // },
 
 
 
@@ -124,12 +199,16 @@ var App = {
 
 var UI = {
 	onClickStart: function(){
-
-		App.setup();
+		// App.setup();
+		// console.log("This are the player cards");
+		// console.log(App.playerCards);
+		App.startGame();
 	},
 
 	onClickHit: function(){
-		App.nextCard();
+		// App.nextCard();
+		// App.hit();
+		// console.log(App.playerCards);
 	},
 
 	onClickStand: function(){
@@ -150,6 +229,7 @@ var UI = {
 
 	onClickReset: function(){
 		console.log("Hello from Reset button");
+		location.reload();
 	},
 }
 
